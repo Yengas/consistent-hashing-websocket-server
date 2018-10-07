8b30c8f7ba1b5d24c5c4ea076d218b882c0f01c2
Yengas <yigitcan@hotmail.com.tr>
2018-10-07 19:22:37 +0300

    added reconnection to the client.

--- client/index.js
+++ client/index.js
@@ -1,15 +1,25 @@
 const url = process.argv[2] || 'http://localhost:8080';
 const room = process.argv[3] || 'default';
-const io = require('socket.io-client')(url, { query: { room } });
 
-io.on('connection', function(){
-  console.log('connected to the server.');
-});
+function connect() {
+	const io = require('socket.io-client')(url, { query: { room } });
 
-io.on('hello', function(data){
-  console.log('got hello', JSON.stringify(data));
-});
+	io.on('connection', function () {
+		console.log('connected to the server.');
+	});
 
-io.on('error', function(err){
-  console.log('got an error', err);
-});
+	io.on('hello', function (data) {
+		console.log('got hello', JSON.stringify(data));
+	});
+
+	io.on('error', function (err) {
+		console.log('got an error', err);
+	});
+
+	io.on('disconnect', function(){
+		console.log('got disconnected, will try re-connecting');
+		return setTimeout(() => connect(), 5000);
+	});
+}
+
+connect();
